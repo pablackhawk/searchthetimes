322fec03c401a19b7f3aa4eb8b667919a6cfdf74
pablackhawk <l.tirtarahardja@gmail.com>
2018-03-04 18:51:11 -0800

    App working, needs styling

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Main from './components/Main';
+
+const App = () => (
+  <Router>
+    <div>
+      <Route path="/" component={Main} />
+    </div>
+  </Router>
+);
+
+export default App;
